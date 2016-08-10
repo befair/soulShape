40806258c292a63a1c7af69eb1dc3ddec3fb2562
radeox <radeox@debian>
2016-08-10 12:12:41 +0200

    [CLEAN] Rimosso tema di base e file di upgrade rimasti

--- wp/soulshape.earth/wp-content/themes/rise-lite/assets/js/rise-lite-customizer.js
+++
@@ -1,8 +0,0 @@
-/**
- *	jQuery Document Ready
- */
-jQuery( document ).ready( function($) {
-	$( window ).load( function() {
-		$( '.zerif-upgrade-to-pro-button' ).attr( 'href', 'http://themeisle.com/themes/zerif-pro-one-page-wordpress-theme?ref=6148' );
-	});
-});
