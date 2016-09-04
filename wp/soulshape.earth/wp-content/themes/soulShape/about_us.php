f04e865973786a35283a53ea25ad2ad99c53e459
radeox <radeox@debian>
2016-09-04 13:41:06 +0200

    [FEATURE] Aggiunta pagina "about us" (vecchia home piu video piÃ leggero, 23mb)

---
+++ wp/soulshape.earth/wp-content/themes/soulShape/about_us.php
@@ -0,0 +1,156 @@
+<?php
+/**
+Template Name: About Us Page
+*/
+get_header(); ?>
+
+<div class="clear"></div>
+
+</header> <!-- / END HOME SECTION  -->
+<?php zerif_after_header_trigger(); ?>
+<div id="content" class="site-content">
+
+	<div>
+
+		<?php zerif_before_page_content_trigger(); ?>
+
+		<div class="content-left-wrap col-md-12" style="padding:0px; padding-top:20px">
+
+			<?php zerif_top_page_content_trigger(); ?>
+
+			<div id="primary" class="content-area">
+				<main id="main" class="site-main">
+
+
+                    <section class="focus" id="focus">
+                        <video width="400" controls>
+                          <source src="http://2.236.89.96/soulShape/wp/soulshape.earth/wp-content/uploads/2016/08/soulShape-Trailer.mp4" type="video/mp4">
+                          Your browser does not support HTML5 video.
+                        </video>
+                    </section>
+
+
+                    <section class="focus" id="focus">
+                        <div>
+                            <div class="section-header">
+                                <h2 class="dark-text">problem we are solving</h2>
+                            </div>
+                            <div class="row">
+                                <div data-sr-complete="true" data-sr-init="true" class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s">
+                                    <div class="service-icon">
+                                        <i class="pixeden" style="background:url(http://2.236.89.96/soulShape/wp/soulshape.earth/wp-content/uploads/2016/06/Indigenous-People-black.png) no-repeat center;width:100%; height:100%;"></i> <!-- FOCUS ICON-->
+                                    </div>
+                                    <h3 class="red-border-bottom">INDIGENOUS PEOPLE</h3>
+                                    <p>“The cultural heritage of different communities, beside their 
+                        ecological value, helps with intercultural dialogue, and encourages 
+                        mutual respect for other ways of life.”</p> 
+                                </div>
+                                <div data-sr-complete="true" data-sr-init="true" class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s">
+                                    <div class="service-icon">
+                                        <i class="pixeden" style="background:url(http://2.236.89.96/soulShape/wp/soulshape.earth/wp-content/uploads/2016/06/old-man-dies.png) no-repeat center;width:100%; height:100%;"></i> <!-- FOCUS ICON-->
+                                    </div>
+                                    <h3 class="red-border-bottom">ELDERLY DIES</h3>
+                                    <p>In 1960, at UNESCO, Malian writer and ethnologist, Amadou Hampåté 
+                    Bå, said: “In Africa, when an old man dies, it’s a library burning”.</p>   
+                                </div>
+                            <div data-sr-complete="true" data-sr-init="true" class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s">
+                                <div class="service-icon">
+                                    <i class="pixeden" style="background:url(http://2.236.89.96/soulShape/wp/soulshape.earth/wp-content/uploads/2016/06/library-burning.png) no-repeat center;width:100%; height:100%;"></i> <!-- FOCUS ICON-->
+                                    </div>
+                                    <h3 class="red-border-bottom">KNOWLEDGE DISAPPEARING </h3>
+                                    <p>Fifty years later, the threat of the extinction of traditional 
+                        knowledge, generally held by the elderly, is even more estreme. For 
+                        example, humanity has already lost many indigenous languages and the 
+                        ecological knowledge and meaning incorporated within them. </p> 
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section class="focus" style="background-color: orange">
+                        <div class="container">
+                            <div class="section-header">
+                                <h2 class="white-text">solution we thought</h2>
+                            </div>
+                        </div>
+                        <div class="container">
+                            <div class="client-list">
+                                <div data-sr-complete="true" data-sr-init="true" class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s">
+                                    <div class="service-icon">
+                                        <i class="pixeden" style="background:url(http://2.236.89.96/soulShape/wp/soulshape.earth/wp-content/uploads/2016/06/3D-scanning.png) no-repeat center;width:100%; height:100%;"></i>
+                                    </div>
+                                    <h3 class="red-border-bottom">3D &amp; TECHNOLOGIES</h3>
+                                    <p>3D laser scanning and photogrammetry are used to collect models of
+                         traditional artifacts and videos are taken to describe the production 
+                        processes and later to facilitate the diffusion on social media. </p>   
+                                </div>
+                                <div data-sr-complete="true" data-sr-init="true" class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s">
+                                    <div class="service-icon">
+                                        <i class="pixeden" style="background:url(http://2.236.89.96/soulShape/wp/soulshape.earth/wp-content/uploads/2016/06/digital-library.png) no-repeat center;width:100%; height:100%;"></i>
+                                    </div>
+                                    <h3 class="red-border-bottom">DIGITAL LIBRARY</h3>
+                                    <p>Data is collected during field missions, and then in crowdsourcing with the participation of indigenous leaders, young and old.</p>  
+                                </div>
+                                <div data-sr-complete="true" data-sr-init="true" class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s">
+                                    <div class="service-icon">
+                                        <i class="pixeden" style="background:url(http://2.236.89.96/soulShape/wp/soulshape.earth/wp-content/uploads/2016/06/preserve-wisdom.png) no-repeat center;width:100%; height:100%;"></i>
+                                    </div>
+                                    <h3 class="red-border-bottom">PRESERVE WISDOM</h3>
+                                    <p>The creation of a digital library preserve indigenous culture and,
+                         on the other, passes down this traditional knowledge to the next 
+                        generations.</p>    
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+
+                    <section class="focus" style="background-color: rgb(0, 128, 128);">
+                        <div class="container">
+                            <div class="section-header">
+                                <h2 class="dark-text">indigenous business model</h2>
+                            </div>
+                            <div data-sr-init="true" style="-webkit-transform: translatex(-60px);-moz-transform: translatex(-60px);-ms-transform: translatex(-60px);transform: translatex(-60px);opacity: 0;" class="row" data-scrollreveal="enter left after 0s over 0.1s">
+                                <div data-sr-init="true" style="-webkit-transform: translatex(-60px);-moz-transform: translatex(-60px);-ms-transform: translatex(-60px);transform: translatex(-60px);opacity: 0;" class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s">
+                                    <div class="service-icon">
+                                        <i class="pixeden" style="background:url(http://2.236.89.96/soulShape/wp/soulshape.earth/wp-content/uploads/2016/06/Indigenous-People-black.png) no-repeat center;width:100%; height:100%;"></i> <!-- FOCUS ICON-->
+                                    </div>
+                                    <h3 class="red-border-bottom">INDIGENOUS PEOPLES</h3>
+                                    <p>Meetings with aboriginals enable us to understand to what extent 
+                        their communities want to expose their material and immaterial culture 
+                        to the 3D imaging market.</p>   
+                                </div>
+                                <div data-sr-init="true" style="-webkit-transform: translatex(-60px);-moz-transform: translatex(-60px);-ms-transform: translatex(-60px);transform: translatex(-60px);opacity: 0;" class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s">
+                                    <div class="service-icon">
+                                        <i class="pixeden" style="background:url(http://2.236.89.96/soulShape/wp/soulshape.earth/wp-content/uploads/2016/06/soulShape-3D.png) no-repeat center;width:100%; height:100%;"></i> <!-- FOCUS ICON-->
+                                    </div>
+                                    <h3 class="red-border-bottom">soulShape 3D MARKET</h3>
+                                    <p>In the 3D imaging market with our vertical offer we differentiate 
+                        for others, however, in a second phase we will also sell handcrafts made
+                         by the indigenous communities. </p>    
+                                </div>
+                                <div data-sr-init="true" style="-webkit-transform: translatex(-60px);-moz-transform: translatex(-60px);-ms-transform: translatex(-60px);transform: translatex(-60px);opacity: 0;" class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s">
+                                    <div class="service-icon">
+                                        <i class="pixeden" style="background:url(http://2.236.89.96/soulShape/wp/soulshape.earth/wp-content/uploads/2016/06/customers.png) no-repeat center;width:100%; height:100%;"></i> <!-- FOCUS ICON-->
+                                    </div>
+                                    <h3 class="red-border-bottom">CUSTOMERS WORLDWIDE</h3>
+                                    <p>They range from video games and animation movie companies needing 
+                            our specific 3D contents to citizens and designers that would like to 
+                            reinterpret using design apps or simply 3D printing in their original 
+                            form. </p>  
+                                </div>
+                            </div> 
+                        </div>
+                    </section>
+                </main>
+			</div>
+
+			<?php zerif_bottom_page_content_trigger(); ?>
+
+		</div><!-- .content-left-wrap -->
+
+		<?php zerif_after_page_content_trigger(); ?>
+
+	</div><!-- .container -->
+
+<?php get_footer(); ?>
